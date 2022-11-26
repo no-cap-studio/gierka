09504d7510fb7a54a931fe1b8b2ddc3518dd3ee1
FckPerv <maksymilkk@gmail.com>
2022-11-26 04:03:34 +0100

    skrypt na warsty spritow dziala
    dobranoc

--- Assets/script/orderControl.cs
+++ Assets/script/orderControl.cs
@@ -8,41 +8,60 @@ using static UnityEngine.GraphicsBuffer;
 
 public class orderControl : MonoBehaviour
 {
-    public float maxRadius;
-    public Collider2D[] overlaps = new Collider2D[50];
-    public GameObject player;
-    public Vector3 spriteSize;
-    void Start()
-    {
-        
-    }
 
-    
-    void Update()
-    {
-        check();
-    }
 
-    public void check()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        int count = Physics2D.OverlapCircleNonAlloc(transform.position, maxRadius, overlaps);
-        for(int i =0; i<overlaps.Length - 1; i++)
+        if (other.gameObject.layer == 3)
         {
-            spriteSize = new Vector3(overlaps[i].GetComponent<SpriteRenderer>().bounds.size.x, overlaps[i].GetComponent<SpriteRenderer>().bounds.size.y, 0.1);
-            if (overlaps[i] != null)
+            if (other.gameObject.transform.position.y > transform.position.y + GetComponent<BoxCollider2D>().offset.y)
             {
-                if (overlaps[i].transform.position.y + overlaps[i].GetComponent<BoxCollider2D>().offset.y > transform.position.y + gameObject.GetComponent<BoxCollider2D>().offset.y )
-                {
-                    player.GetComponent<SpriteRenderer>().sortingOrder = overlaps[i].GetComponent<SpriteRenderer>().sortingOrder + 1;
-                }
-                else
-                {
-                    player.GetComponent<SpriteRenderer>().sortingOrder = overlaps[i].GetComponent<SpriteRenderer>().sortingOrder - 1;
-                }
+                other.gameObject.GetComponent<SpriteRenderer>().sortingOrder = gameObject.GetComponent<SpriteRenderer>().sortingOrder - 1;
+
             }
-            
+            else
+            {
+                other.gameObject.GetComponent<SpriteRenderer>().sortingOrder = gameObject.GetComponent<SpriteRenderer>().sortingOrder + 1;
+
+            }
         }
-            
-        
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 3)
+        {
+            if (other.gameObject.transform.position.y > transform.position.y + GetComponent<BoxCollider2D>().offset.y)
+            {
+                other.gameObject.GetComponent<SpriteRenderer>().sortingOrder = gameObject.GetComponent<SpriteRenderer>().sortingOrder - 1;
+
+            }
+            else
+            {
+                other.gameObject.GetComponent<SpriteRenderer>().sortingOrder = gameObject.GetComponent<SpriteRenderer>().sortingOrder + 1;
+
+            }
+        }
+    }
+
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 3)
+        {
+            if (other.gameObject.transform.position.y > transform.position.y + GetComponent<BoxCollider2D>().offset.y)
+            {
+                other.gameObject.GetComponent<SpriteRenderer>().sortingOrder = gameObject.GetComponent<SpriteRenderer>().sortingOrder - 1;
+
+            }
+            else
+            {
+                other.gameObject.GetComponent<SpriteRenderer>().sortingOrder = gameObject.GetComponent<SpriteRenderer>().sortingOrder + 1;
+
+            }
+        }
     }
 }
+
+
+
