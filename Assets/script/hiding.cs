8c40f7272236c92f2267df5fb2817da6d403b305
FckPerv <78822364+FckPerv@users.noreply.github.com>
2023-02-13 01:01:48 +0100

    nowa Mechanika chowania cutscenka tez siedzi

--- Assets/script/hiding.cs
+++ Assets/script/hiding.cs
@@ -12,10 +12,16 @@ public class hiding : MonoBehaviour
     public Sprite unHidden;
     public Sprite hidden;
     SpriteRenderer sprite;
+    public GameObject leftjasper;
+    public GameObject rightjasper;
+    public GameObject upjasper;
+    public GameObject downjasper;
+    GameObject selected;
     void Start()
     {
         move = player.GetComponent<playerMovement>();
         sprite = GetComponent<SpriteRenderer>();
+
     }
 
     public void hidinges()
@@ -33,14 +39,114 @@ public class hiding : MonoBehaviour
     {
         if (move.isHiding == true && Input.GetKeyDown(KeyCode.F))
         {
+            if (selected != null)
+            {
+                player.transform.position = selected.transform.position;
+            }
+
             player.GetComponent<SpriteRenderer>().enabled = true;
             player.tag = "Player";
             sprite.sprite = unHidden;
             move.isHiding = false;
             hideIcon.gameObject.SetActive(true);
             player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+
+            if (downjasper != null)
+            {
+                downjasper.SetActive(false);
+            }
+            if (upjasper != null)
+            {
+                upjasper.SetActive(false);
+            }
+            if (leftjasper != null)
+            {
+                leftjasper.SetActive(false);
+            }
+            if (rightjasper != null)
+            {
+                rightjasper.SetActive(false);
+            }
+            selected = null;
         }
         
+    }
+
+    public void choosingPosition()
+    {
+        if (leftjasper != null && Input.GetKey(KeyCode.A)) {
+
+            leftjasper.SetActive(true);
+            if (rightjasper != null)
+            {
+                rightjasper.SetActive(false);
+            }
+            if (upjasper != null)
+            {
+                upjasper.SetActive(false);
+            }
+            if (downjasper != null)
+            {
+                downjasper.SetActive(false);
+            }          
+            selected = leftjasper;
+
+
+        }
+        else if (rightjasper != null && Input.GetKey(KeyCode.D)) {
+
+            if (leftjasper != null)
+            {
+                leftjasper.SetActive(false);
+            }
+            
+            rightjasper.SetActive(true);
+
+            if (upjasper != null)
+            {
+                upjasper.SetActive(false);
+            }
+
+            if (downjasper != null)
+            {
+                downjasper.SetActive(false);
+            }
+
+            selected = rightjasper;
+        }
+        else if (upjasper != null && Input.GetKey(KeyCode.W)) {
+
+            if (leftjasper != null)
+            {
+                leftjasper.SetActive(false);
+            }
+            if (rightjasper != null)
+            {
+                rightjasper.SetActive(false);
+            }
+            upjasper.SetActive(true);
+            if (downjasper != null)
+            {
+                downjasper.SetActive(false);
+            }
+            selected = upjasper;
+        }
+        else if (downjasper != null && Input.GetKey(KeyCode.S)) {
+            if (leftjasper != null)
+            {
+                leftjasper.SetActive(false);
+            }
+            if (rightjasper != null)
+            {
+                rightjasper.SetActive(false);
+            }
+            if (upjasper != null)
+            {
+                upjasper.SetActive(false);
+            }
+            downjasper.SetActive(true);
+            selected = downjasper;
+        }     
     }
 
     private void OnTriggerStay2D(Collider2D collision)
